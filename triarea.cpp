ce56288ab05e559bd8cd877118e0fd2d02c20ae3
justauser897 <71141157+justauser897@users.noreply.github.com>
2020-12-10 08:55:33 +0200

    Add files via upload

---
+++ triarea.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int b, h, area;
+	std::cout <<"Enter the base and height of the triangle. ";
+	std::cin >> b;
+	std::cin >> h;
+	area=(b*h)/2;
+	std::cout <<"The area is "<< area ;
+	
+	return 0;
+}
